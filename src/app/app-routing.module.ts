05b80e5da2e55e27958a89077e15545b462c82f6
AlissonAnjos21 <93294707+AlissonAnjos21@users.noreply.github.com>
2022-10-17 01:26:47 -0300

    Login (Login) - Creating a new module, he is the login module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  // { path: '', pathMatch: 'full', redirectTo: 'administradores' },
+  { path: '', pathMatch: 'full', redirectTo: 'login' },
+  { path: 'login',
+   loadChildren: () => import('./login/login.module').then(m => m.LoginModule)
+  },
   { path: 'administradores',
     loadChildren: () => import('./administrador/administrador.module').then(m => m.AdministradorModule)
   }
