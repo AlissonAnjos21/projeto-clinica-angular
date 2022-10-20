18a0ffa70d177561e042674e26be7995d3e95bb2
AlissonAnjos21 <93294707+AlissonAnjos21@users.noreply.github.com>
2022-10-20 00:12:30 -0300

    Appointment base (Consulta) - Base structure from appointment's module

--- src/app/consulta/consulta.module.ts
+++ src/app/consulta/consulta.module.ts
@@ -2,10 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ConsultaRoutingModule } from './consulta-routing.module';
+import { ConsultaListComponent } from './components/consulta-list/consulta-list.component';
+import { ConsultaComponent } from './containers/consulta/consulta.component';
+import { ConsultaFormComponent } from './containers/consulta-form/consulta-form.component';
+import { ConsultaSearchComponent } from './containers/consulta-search/consulta-search.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ConsultaListComponent,
+    ConsultaComponent,
+    ConsultaFormComponent,
+    ConsultaSearchComponent
+  ],
   imports: [
     CommonModule,
     ConsultaRoutingModule
---
+++ src/app/consulta/containers/consulta-search/consulta-search.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-consulta-search',
+  templateUrl: './consulta-search.component.html',
+  styleUrls: ['./consulta-search.component.css']
+})
+export class ConsultaSearchComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
