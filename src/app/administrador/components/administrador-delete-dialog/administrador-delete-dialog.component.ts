792220e316bc59ed01f56b110cf56c08073c6bda
AlissonAnjos21 <93294707+AlissonAnjos21@users.noreply.github.com>
2022-10-20 05:40:48 -0300

    Form (ConsultaForm) - Appointment's form

--- src/app/administrador/components/administrador-delete-dialog/administrador-delete-dialog.component.ts
+++ src/app/administrador/components/administrador-delete-dialog/administrador-delete-dialog.component.ts
@@ -2,6 +2,7 @@ import { Component, Inject, OnInit } from '@angular/core';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { AdministradorService } from '../../services/administrador.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-administrador-delete-dialog',
@@ -13,7 +14,8 @@ export class AdministradorDeleteDialogComponent implements OnInit {
   constructor(
     @Inject(MAT_DIALOG_DATA) public data: string,
     private serviceAdministrador: AdministradorService,
-    private _snackBar: MatSnackBar
+    private _snackBar: MatSnackBar,
+    private router: Router
     ) {
       // console.log(data);
     }
@@ -27,12 +29,19 @@ export class AdministradorDeleteDialogComponent implements OnInit {
   }
 
   private onSuccess() {
-    window.location.reload();
+    this.reloadComponent(true);
     this._snackBar.open('Remoção realizada com sucesso!', '', {duration: 5000});
   }
 
   private onError() {
     this._snackBar.open('Falha ao remover item! Tente novamente.', '', {duration: 5000});
+  }
+
+  private reloadComponent(self: boolean, urlToNavigateTo?: string){
+    const url = self ? this.router.url : urlToNavigateTo;
+    this.router.navigateByUrl('/',{skipLocationChange:true}).then(()=>{
+      this.router.navigate([`/${url}`]);
+    });
   }
 
 }
--- src/app/consulta/components/consulta-delete-dialog/consulta-delete-dialog.component.ts
+++ src/app/consulta/components/consulta-delete-dialog/consulta-delete-dialog.component.ts
@@ -2,6 +2,7 @@ import { Component, Inject, OnInit } from '@angular/core';
 import { ConsultaService } from '../../services/consulta.service';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-consulta-delete-dialog',
@@ -13,7 +14,8 @@ export class ConsultaDeleteDialogComponent implements OnInit {
   constructor(
     @Inject(MAT_DIALOG_DATA) public data: string,
     private serviceConsulta: ConsultaService,
-    private _snackBar: MatSnackBar
+    private _snackBar: MatSnackBar,
+    private router: Router
     ) {
       // console.log(data);
     }
@@ -27,12 +29,19 @@ export class ConsultaDeleteDialogComponent implements OnInit {
   }
 
   private onSuccess() {
-    window.location.reload();
+    this.reloadComponent(true);
     this._snackBar.open('Remoção realizada com sucesso!', '', {duration: 5000});
   }
 
   private onError() {
     this._snackBar.open('Falha ao remover item! Tente novamente.', '', {duration: 5000});
+  }
+
+  private reloadComponent(self: boolean, urlToNavigateTo?: string){
+    const url = self ? this.router.url : urlToNavigateTo;
+    this.router.navigateByUrl('/',{skipLocationChange:true}).then(()=>{
+      this.router.navigate([`/${url}`]);
+    });
   }
 
 }
