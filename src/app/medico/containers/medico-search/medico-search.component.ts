9dee371d39d709026408e34d499c0df56b835aba
AlissonAnjos21 <93294707+AlissonAnjos21@users.noreply.github.com>
2022-10-22 01:03:59 -0300

    Search (MedicoSearch) - Doctor's search component

--- src/app/medico/containers/medico-search/medico-search.component.ts
+++ src/app/medico/containers/medico-search/medico-search.component.ts
@@ -1,4 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { Medico } from '../../model/medico';
+import { Observable, of, catchError, toArray } from 'rxjs';
+import { MedicoService } from '../../services/medico.service';
+import { NonNullableFormBuilder } from '@angular/forms';
+import { MatDialog } from '@angular/material/dialog';
+import { Router } from '@angular/router';
+import { LoginService } from '../../../login/services/login.service';
+import { Location } from '@angular/common';
+import { ErrorDialogComponent } from '../../../shared/components/error-dialog/error-dialog.component';
+import { MedicoDeleteDialogComponent } from '../../components/medico-delete-dialog/medico-delete-dialog.component';
 
 @Component({
   selector: 'app-medico-search',
@@ -7,9 +17,76 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MedicoSearchComponent implements OnInit {
 
-  constructor() { }
+  formSearchType = this.formBuilder.group({
+    type: [''],
+    value: ['']
+  });
+
+  medicos$: Observable<Medico[]>;
+
+  constructor(
+    private formBuilder: NonNullableFormBuilder,
+    private serviceMedico: MedicoService,
+    public dialog: MatDialog,
+    private router: Router,
+    private location: Location,
+    private login: LoginService
+  ) {
+    this.medicos$ = new Observable<Medico[]>();
+   }
 
   ngOnInit(): void {
+    this.login.loginTest();
+  }
+
+  onSubmit() {
+    if(this.formSearchType.value.type == 'id' && this.formSearchType.value.value){
+      this.medicos$ = this.serviceMedico.findById(this.formSearchType.value.value!).pipe(
+        toArray(),
+        catchError(error => {
+          this.onError('Falha na Exibição! Verifique a base de dados.');
+          return of();
+        })
+      );
+
+    }else if(this.formSearchType.value.type == 'nome' && this.formSearchType.value.value){
+      this.medicos$ = this.serviceMedico.findByNome(this.formSearchType.value.value!).pipe(
+        catchError(error => {
+          this.onError('Falha na Exibição! Verifique a base de dados.');
+          return of([]);
+        }));
+
+    }else if(this.formSearchType.value.type == 'cpf' && this.formSearchType.value.value){
+      this.medicos$ = this.serviceMedico.findByCpf(this.formSearchType.value.value!).pipe(
+        catchError(error => {
+          this.onError('Falha na Exibição! Verifique a base de dados.');
+          return of([]);
+        }));
+
+    }else {
+      this.onError('Falha na Exibição! Insira os dados necessários.');
+    }
+
+  }
+
+  onAdd() {
+    this.router.navigate(['medicos/new']);
+  }
+
+  onEdit(medico: Medico) {
+    this.router.navigate(['medicos/edit', medico.id]);
+  }
+
+  onDelete(medico: Medico) {
+    this.dialog.open(MedicoDeleteDialogComponent, {data: medico.id});
+  }
+
+  private onError(errorMsg: string) {
+    this.dialog.open(ErrorDialogComponent, {data: errorMsg});
+  }
+
+  onBack() {
+    this.location.back();
   }
 
 }
