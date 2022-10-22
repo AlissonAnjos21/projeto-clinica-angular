3a3b39b5587fe984fbca64fb3861b4e30f8f5014
AlissonAnjos21 <93294707+AlissonAnjos21@users.noreply.github.com>
2022-10-21 21:18:47 -0300

    Service (MedicoService) - Implementing the doctor's service

--- src/app/medico/services/medico.service.ts
+++ src/app/medico/services/medico.service.ts
@@ -1,9 +1,50 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Medico } from '../model/medico';
+import { first } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MedicoService {
 
-  constructor() { }
+  private readonly API = 'api/medicos';
+
+  constructor(private httpClient: HttpClient) { }
+
+  listAll() {
+    return this.httpClient.get<Medico[]>(this.API).pipe(first());
+  }
+
+  findById(id: string) {
+    return this.httpClient.get<Medico>(`${this.API}/${id}`);
+  }
+
+  findByNome(nome: string) {
+    return this.httpClient.get<Medico[]>(`${this.API}/find?nome=${nome}`);
+  }
+
+  findByCpf(nome: string) {
+    return this.httpClient.get<Medico[]>(`${this.API}/find?cpf=${nome}`);
+  }
+
+  save(medico: Partial<Medico>) {
+    if(medico.id) {
+      return this.update(medico);
+    }
+    return this.create(medico);
+  }
+
+  private create(medico: Partial<Medico>) {
+    return this.httpClient.post<Medico>(this.API, medico);
+  }
+
+  private update(medico: Partial<Medico>) {
+    return this.httpClient.put<Medico>(this.API, medico);
+  }
+
+  delete(id: string) {
+    return this.httpClient.delete(`${this.API}/${id}`).pipe(first());
+  }
+
 }
