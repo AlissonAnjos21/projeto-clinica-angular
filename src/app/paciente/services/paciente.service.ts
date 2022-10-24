592a7727a2906d806aa45ebd27760b7cb59aa33b
AlissonAnjos21 <93294707+AlissonAnjos21@users.noreply.github.com>
2022-10-24 00:11:41 -0300

    Service (PacienteService) - Configuring the patient's service

--- src/app/paciente/services/paciente.service.ts
+++ src/app/paciente/services/paciente.service.ts
@@ -1,9 +1,50 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Paciente } from '../model/paciente';
+import { first } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PacienteService {
 
-  constructor() { }
+  private readonly API = 'api/pacientes';
+
+  constructor(private httpClient: HttpClient) { }
+
+  listAll() {
+    return this.httpClient.get<Paciente[]>(this.API).pipe(first());
+  }
+
+  findById(id: string) {
+    return this.httpClient.get<Paciente>(`${this.API}/${id}`);
+  }
+
+  findByNome(nome: string) {
+    return this.httpClient.get<Paciente[]>(`${this.API}/find?nome=${nome}`);
+  }
+
+  findByCpf(nome: string) {
+    return this.httpClient.get<Paciente[]>(`${this.API}/find?cpf=${nome}`);
+  }
+
+  save(paciente: Partial<Paciente>) {
+    if(paciente.id) {
+      return this.update(paciente);
+    }
+    return this.create(paciente);
+  }
+
+  private create(paciente: Partial<Paciente>) {
+    return this.httpClient.post<Paciente>(this.API, paciente);
+  }
+
+  private update(paciente: Partial<Paciente>) {
+    return this.httpClient.put<Paciente>(this.API, paciente);
+  }
+
+  delete(id: string) {
+    return this.httpClient.delete(`${this.API}/${id}`).pipe(first());
+  }
+
 }
