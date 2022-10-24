85ac1b204b607590e56da3cc05ebe06653cf7e9f
AlissonAnjos21 <93294707+AlissonAnjos21@users.noreply.github.com>
2022-10-24 05:09:33 -0300

    Patient's View (PacienteView) - Configuring patient's view component

--- src/app/paciente/components/paciente-view/paciente-view.component.ts
+++ src/app/paciente/components/paciente-view/paciente-view.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { Paciente } from '../../model/paciente';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-paciente-view',
@@ -7,7 +9,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PacienteViewComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    @Inject(MAT_DIALOG_DATA) public data: Paciente
+  ) { }
 
   ngOnInit(): void {
   }
