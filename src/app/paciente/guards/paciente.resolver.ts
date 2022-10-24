82676a6cfdf194ed25e9712b70070190dd4adeb8
AlissonAnjos21 <93294707+AlissonAnjos21@users.noreply.github.com>
2022-10-24 00:27:29 -0300

    Resolver (PacienteResolver) - Configuring patient's resolver

--- src/app/paciente/guards/paciente.resolver.ts
+++ src/app/paciente/guards/paciente.resolver.ts
@@ -1,16 +1,25 @@
 import { Injectable } from '@angular/core';
 import {
-  Router, Resolve,
+  Resolve,
   RouterStateSnapshot,
   ActivatedRouteSnapshot
 } from '@angular/router';
 import { Observable, of } from 'rxjs';
+import { PacienteService } from '../services/paciente.service';
+import { Paciente } from '../model/paciente';
 
 @Injectable({
   providedIn: 'root'
 })
-export class PacienteResolver implements Resolve<boolean> {
-  resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> {
-    return of(true);
+export class PacienteResolver implements Resolve<Paciente> {
+
+  constructor(private servicePaciente: PacienteService) {}
+
+  resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<Paciente> {
+    if(route.params && route.params['id']) {
+      return this.servicePaciente.findById(route.params['id']);
+    }
+
+    return of({id: '', nome: '', cpf: '', email: '', dataNascimento: '', telefone: '', telefoneFamiliar: '', ocupacao: '', endereco: '', planoSaude: '', alergias: [''], doencas: ['']});
   }
 }
