7ad28e02249dff90f0055b9a8309613f2dce043b
AlissonAnjos21 <93294707+AlissonAnjos21@users.noreply.github.com>
2022-10-24 00:32:13 -0300

    Delete Dialog (PacienteDeleteDialog) - Configuring patient's delete dialog

--- src/app/paciente/components/paciente-delete-dialog/paciente-delete-dialog.component.ts
+++ src/app/paciente/components/paciente-delete-dialog/paciente-delete-dialog.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { PacienteService } from '../../services/paciente.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-paciente-delete-dialog',
@@ -7,9 +11,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PacienteDeleteDialogComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    @Inject(MAT_DIALOG_DATA) public data: string,
+    private servicePaciente: PacienteService,
+    private _snackBar: MatSnackBar,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  onAgree() {
+    this.servicePaciente.delete(this.data).subscribe({next: result => {this.onSuccess();}, error: error => {this.onError();}});
+
+  }
+
+  private onSuccess() {
+    this.reloadComponent(true);
+    this._snackBar.open('Remoção realizada com sucesso!', '', {duration: 5000});
+  }
+
+  private onError() {
+    this._snackBar.open('Falha ao remover item! Tente novamente.', '', {duration: 5000});
+  }
+
+  private reloadComponent(self: boolean, urlToNavigateTo?: string){
+    const url = self ? this.router.url : urlToNavigateTo;
+    this.router.navigateByUrl('/',{skipLocationChange:true}).then(()=>{
+      this.router.navigate([`/${url}`]);
+    });
   }
 
 }
